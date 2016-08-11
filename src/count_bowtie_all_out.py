d5d131808fa39fb2865fa3216fdb2a7408d837ea
logstar <y.will.zhang@gmail.com>
2016-08-11 18:01:32 -0400

    Revised stats of count_bowtie_all_out.py.

--- src/count_bowtie_all_out.py
+++ src/count_bowtie_all_out.py
@@ -4,13 +4,13 @@ import fastautil
 import bowtieutil as bu
 
 def parse_bt_file(in_bt_fn, qcutoff, ref_length):
-    num_uniq_aligned = 0
+    num_all_aligned = 0
     num_qual_failed = 0
 
     bt_align_counter = bu.BowtieRecordCounter(ref_length)
     #{position : {'+' : count, '-' : count}, ...}
     for bt_rec_ntup in bu.iterate_bowtie_out_file(in_bt_fn):
-        num_uniq_aligned += 1
+        num_all_aligned += 1
         assert bt_rec_ntup.mm_desc == '', ("Read alignment has mismatches. %s" % 
                                            bt_rec_ntup._asdict())
 
@@ -18,19 +18,19 @@ def parse_bt_file(in_bt_fn, qcutoff, ref_length):
             num_qual_failed += 1
         else:
             bt_align_counter.insert_bt_rec_ntup(bt_rec_ntup)
-    return [bt_align_counter, num_uniq_aligned, num_qual_failed]
+    return [bt_align_counter, num_all_aligned, num_qual_failed]
 
 def write_parse_bt_stats(stats_list, stats_fn):
-    num_uniq_aligned = stats_list[0]
+    num_all_aligned = stats_list[0]
     num_qual_failed = stats_list[1]
-    num_filtered = num_uniq_aligned - num_qual_failed
+    num_filtered = num_all_aligned - num_qual_failed
 
     stats_file = open(stats_fn, 'w')
-    stats_file.write("Number of reads uniquely aligned:%d\n" % num_uniq_aligned)
+    stats_file.write("Number of all alignments:%d\n" % num_all_aligned)
     stats_file.write("Number of reads with quality socre <= cutoff: %d (%s%%)\n" 
-                      % (num_qual_failed, "{:.4f}".format(float(num_qual_failed) / num_uniq_aligned * 100)))
-    stats_file.write("Number of uniquely aligned reads passed quality socre filter: %d (%s%%)\n" 
-                      % (num_filtered, "{:.4f}".format(float(num_filtered) / num_uniq_aligned * 100)))
+                      % (num_qual_failed, "{:.4f}".format(float(num_qual_failed) / num_all_aligned * 100)))
+    stats_file.write("Number of all alignments passed quality socre filter: %d (%s%%)\n" 
+                      % (num_filtered, "{:.4f}".format(float(num_filtered) / num_all_aligned * 100)))
 
     stats_file.close()
 
@@ -38,7 +38,8 @@ def main():
     argv = sys.argv
     if len(argv) != 6:
         sys.stderr.write('Usage:\n%s [>= quality score] <input reference genome file name> \
-<input bowtie output> <output stats file> <output start position count file 0 based>\n' % argv[0])
+<input bowtie output (allow alignment to multiple locations)> \
+<output stats file> <output start position count file 0 based>\n' % argv[0])
         return -2
 
     qc = int(argv[1])
