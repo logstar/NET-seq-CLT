509f675ad3389167773db5307f5fa05797739f48
logstar <y.will.zhang@gmail.com>
2017-09-16 17:16:24 -0400

    get_stranded_circularized_seq()
    Extract sequence with coord, number of upstream bp, number of downstream
    bp, and strand.
    
    Treat the genome as circular.

--- src/fastautil.py
+++ src/fastautil.py
@@ -1,4 +1,6 @@
 # python2.7 notice: use of integer division. 
+import dnautil
+
 class SingleEntryFastaFile(object):
     """Simple class for manipulating single record fasta file"""
     def __init__(self, file_path):
@@ -13,17 +15,81 @@ class SingleEntryFastaFile(object):
         return self.ref_seq[start_ind:end_ind]
 
     def get_circularized_seq(self, start_ind, num_bp):
+        if start_ind < 0 or start_ind >= self.ref_seq_len:
+            raise ValueError("start_ind should >= 0: %s" % start_ind)
+            
         if num_bp < 0:
-            raise ValueError("num_bp must be positive")
+            raise ValueError("num_bp should >= 0: %s" % num_bp)
+
         end_ind = start_ind + num_bp
         if end_ind <= len(self.ref_seq):
-            return self.ref_seq[start_ind:end_ind]
+            # queried sequence
+            q_seq = self.ref_seq[start_ind:end_ind]
         else:
             # when num_bp covers more than 2 copies of whole sequence
             extra_bp_after_one_complete_seq = end_ind - len(self.ref_seq)
             num_whole_seq = extra_bp_after_one_complete_seq / len(self.ref_seq)
             num_trail_bp = extra_bp_after_one_complete_seq % len(self.ref_seq)
-            return self.ref_seq[start_ind:] + self.ref_seq * num_whole_seq + self.ref_seq[:num_trail_bp]
+            q_seq = self.ref_seq[start_ind:] + self.ref_seq * num_whole_seq + self.ref_seq[:num_trail_bp]
+
+        assert len(q_seq) == num_bp
+        return q_seq
+        
+    def get_stranded_circularized_seq(self, coord, up_bp, down_bp, strand):
+        assert coord >= 0 and coord < self.ref_seq_len
+        assert up_bp >= 0
+        assert down_bp >= 0
+        
+        if strand == '+':
+            left_bp = up_bp
+            right_bp = down_bp
+        elif strand == '-':
+            left_bp = down_bp
+            right_bp = up_bp
+        else:
+            raise ValueError("Unknown strand type: %s" % strand)
+
+        # coord nt is the + strand nt
+        # + strand right sequence can directly be extracted using self.get_circularized_seq()
+        c_right_seq = self.get_circularized_seq(coord, right_bp + 1)
+        c_nt = c_right_seq[0]
+        right_seq = c_right_seq[1:]
+
+        # + strand left seq
+        # left seq start index might be < 0
+        # abs_left_ind is the number of extra bp needed before the 
+        # reference start
+        # ACGTACG
+        # 0123456
+        # coord = 1
+        # left_bp = 10
+        # left_ind = -9, need 9 bp before the reference start
+        # num_end_to_start_bp = abs(left_ind) % ref_seq_len = 9 % 7 = 2
+        # This is the number of bp needed from the ref end to start.
+        #
+        # 0 <= num_end_to_start_bp <= ref_seq_len - 1
+        # ACGTACT
+        # 0654321
+        left_ind = coord - left_bp
+        if left_ind < 0:
+            abs_left_ind = abs(left_ind)
+            num_end_to_start_bp = abs_left_ind % self.ref_seq_len
+            if num_end_to_start_bp != 0:
+                left_ind = self.ref_seq_len - num_end_to_start_bp
+            else:
+                left_ind = 0
+        
+        left_seq = self.get_circularized_seq(left_ind, left_bp)
+
+        lcr_seq = left_seq.upper() + c_nt.lower() + right_seq
+
+        if strand == '+':
+            return lcr_seq
+        elif strand == '-':
+            return  dnautil.reverse_complement(lcr_seq)
+        else:
+            raise ValueError("Unknown strand type: %s" % strand)
+
 
     @staticmethod
     def get_fasta_header_seq(file_path):
--- testing/test_fastautil.py
+++ testing/test_fastautil.py
@@ -2,6 +2,7 @@
 import unittest
 import env
 import src.fastautil as fa
+import src.dnautil as dnautil
 
 class TestSingleEntryFastaFile(unittest.TestCase):
     def test_seq_len(self):
@@ -36,6 +37,7 @@ class TestSingleEntryFastaFile(unittest.TestCase):
             tfa_file.write('ACGTA' * 10 + '\n')
             tfa_file.write('C' * 16 + '\n')
         test_fa_instance = fa.SingleEntryFastaFile(tfa_fn)
+
         self.assertEqual(test_fa_instance.get_circularized_seq(0, 5), 'ACGTA')
         self.assertEqual(test_fa_instance.get_circularized_seq(5, 5), 'ACGTA')
         self.assertEqual(test_fa_instance.get_circularized_seq(3, 7), 'TAACGTA')
@@ -55,6 +57,62 @@ class TestSingleEntryFastaFile(unittest.TestCase):
         self.assertEqual(test_fa_instance.get_circularized_seq(45, 21 + 69), 'ACGTA' + 'C' * 16 + test_fa_instance.ref_seq + 'ACG')
         self.assertEqual(test_fa_instance.get_circularized_seq(45, 21 + 70), 'ACGTA' + 'C' * 16 + test_fa_instance.ref_seq + 'ACGT')
         self.assertEqual(test_fa_instance.get_circularized_seq(45, 21 + 71), 'ACGTA' + 'C' * 16 + test_fa_instance.ref_seq + 'ACGTA')
+
+    def test_get_stranded_circularized_seq(self):
+        tfa_fn = 'data/test_fautil2.fna'
+        with open(tfa_fn, 'w') as tfa_file:
+            tfa_file.write('>Test\n')
+            tfa_file.write('ACGTA' * 10 + '\n')
+            tfa_file.write('C' * 16 + '\n')
+        test_fa_instance = fa.SingleEntryFastaFile(tfa_fn)
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(0, 0, 5 - 1, '+'), 'aCGTA')
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(5, 0, 5 - 1, '+'), 'aCGTA')
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(3, 0, 7 - 1, '+'), 'tAACGTA')
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(50, 0, 5 - 1, '+'), 'cCCCC')
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(45, 0, 10 - 1, '+'), 'aCGTACCCCC')
+
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(45, 0, 21 - 1, '+'), 'aCGTA' + 'C' * 16)
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(45, 0, 22 - 1, '+'), 'aCGTA' + 'C' * 16 + 'A')
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(45, 0, 23 - 1, '+'), 'aCGTA' + 'C' * 16 + 'AC')
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(45, 0, 24 - 1, '+'), 'aCGTA' + 'C' * 16 + 'ACG')
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(45, 0, 25 - 1, '+'), 'aCGTA' + 'C' * 16 + 'ACGT')
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(45, 0, 26 - 1, '+'), 'aCGTA' + 'C' * 16 + 'ACGTA')
+
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(45, 0, 21 + 66 - 1, '+'), 'aCGTA' + 'C' * 16 + test_fa_instance.ref_seq)
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(45, 0, 21 + 67 - 1, '+'), 'aCGTA' + 'C' * 16 + test_fa_instance.ref_seq + 'A')
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(45, 0, 21 + 68 - 1, '+'), 'aCGTA' + 'C' * 16 + test_fa_instance.ref_seq + 'AC')
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(45, 0, 21 + 69 - 1, '+'), 'aCGTA' + 'C' * 16 + test_fa_instance.ref_seq + 'ACG')
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(45, 0, 21 + 70 - 1, '+'), 'aCGTA' + 'C' * 16 + test_fa_instance.ref_seq + 'ACGT')
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(45, 0, 21 + 71 - 1, '+'), 'aCGTA' + 'C' * 16 + test_fa_instance.ref_seq + 'ACGTA')
+
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(0, 5 - 1, 0, '-'), dnautil.reverse_complement('aCGTA'))
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(5, 5 - 1, 0, '-'), dnautil.reverse_complement('aCGTA'))
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(3, 7 - 1, 0, '-'), dnautil.reverse_complement('tAACGTA'))
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(50, 5 - 1, 0, '-'), dnautil.reverse_complement('cCCCC'))
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(45, 10 - 1, 0, '-'), dnautil.reverse_complement('aCGTACCCCC'))
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(45, 21 - 1, 0, '-'), dnautil.reverse_complement('aCGTA' + 'C' * 16))
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(45, 22 - 1, 0, '-'), dnautil.reverse_complement('aCGTA' + 'C' * 16 + 'A'))
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(45, 23 - 1, 0, '-'), dnautil.reverse_complement('aCGTA' + 'C' * 16 + 'AC'))
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(45, 24 - 1, 0, '-'), dnautil.reverse_complement('aCGTA' + 'C' * 16 + 'ACG'))
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(45, 25 - 1, 0, '-'), dnautil.reverse_complement('aCGTA' + 'C' * 16 + 'ACGT'))
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(45, 26 - 1, 0, '-'), dnautil.reverse_complement('aCGTA' + 'C' * 16 + 'ACGTA'))
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(45, 21 + 66 - 1, 0, '-'), dnautil.reverse_complement('aCGTA' + 'C' * 16 + test_fa_instance.ref_seq))
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(45, 21 + 67 - 1, 0, '-'), dnautil.reverse_complement('aCGTA' + 'C' * 16 + test_fa_instance.ref_seq + 'A'))
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(45, 21 + 68 - 1, 0, '-'), dnautil.reverse_complement('aCGTA' + 'C' * 16 + test_fa_instance.ref_seq + 'AC'))
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(45, 21 + 69 - 1, 0, '-'), dnautil.reverse_complement('aCGTA' + 'C' * 16 + test_fa_instance.ref_seq + 'ACG'))
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(45, 21 + 70 - 1, 0, '-'), dnautil.reverse_complement('aCGTA' + 'C' * 16 + test_fa_instance.ref_seq + 'ACGT'))
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(45, 21 + 71 - 1, 0, '-'), dnautil.reverse_complement('aCGTA' + 'C' * 16 + test_fa_instance.ref_seq + 'ACGTA'))
+
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(0, 4, 4, '+'), 'CCCCaCGTA')
+
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(0, 4, 4, '-'), dnautil.reverse_complement('CCCCaCGTA'))
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(0, 4, 1, '-'), dnautil.reverse_complement('CaCGTA'))
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(0, 4, test_fa_instance.ref_seq_len, '-'), dnautil.reverse_complement(test_fa_instance.ref_seq + 'aCGTA'))
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(0, 4, 2 * test_fa_instance.ref_seq_len, '-'), dnautil.reverse_complement(2 * test_fa_instance.ref_seq + 'aCGTA'))
+
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(0, 4, 1, '+'), 'CCCCaC')
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(0, 4, test_fa_instance.ref_seq_len, '+'), 'CCCCa' + test_fa_instance.ref_seq[1:] + 'A')
+        self.assertEqual(test_fa_instance.get_stranded_circularized_seq(0, 4, 2 * test_fa_instance.ref_seq_len, '+'), 'CCCCa' + test_fa_instance.ref_seq[1:] + test_fa_instance.ref_seq + 'A')
 
 
 if __name__ == '__main__':
