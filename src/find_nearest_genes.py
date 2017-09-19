417725835dc139bc189bfe5e639227f0c85f86ad
logstar <y.will.zhang@gmail.com>
2017-09-18 21:20:32 -0400

    use argparse

--- src/find_nearest_genes.py
+++ src/find_nearest_genes.py
@@ -1,39 +1,50 @@
 #!/usr/bin/env python2.7
 import sys
 from gffutils import GFF
+import argparse
+
 def find_nearest_genes(in_tss_fn, o_tss_fn, ref_gff):
-	in_tss_file = open(in_tss_fn, 'r')
-	o_tss_file = open(o_tss_fn, 'w')
+    in_tss_file = open(in_tss_fn, 'r')
+    o_tss_file = open(o_tss_fn, 'w')
 
-	for line in in_tss_file:
-		fields = line[:-1].split('\t')
-		coord = int(fields[0])
-		strand = fields[1]
-		nearest_upstream_gene_tup = ref_gff.get_nearest_upstream_gene(coord, strand)
-		nearest_downstream_gene_tup = ref_gff.get_nearest_downstream_gene(coord, strand)
+    for line in in_tss_file:
+        fields = line[:-1].split('\t')
+        coord = int(fields[0])
+        strand = fields[1]
+        nearest_upstream_gene_tup = ref_gff.get_nearest_upstream_gene(coord, strand)
+        nearest_downstream_gene_tup = ref_gff.get_nearest_downstream_gene(coord, strand)
 
-		o_tss_file.write(line.strip())
+        o_tss_file.write(line.strip())
 
-		o_tss_file.write("\t%d\t" % nearest_upstream_gene_tup[0])
-		o_tss_file.write('|'.join(map(lambda tup: tup[1][:-1].replace('\t', ' '), nearest_upstream_gene_tup[1])))
-		o_tss_file.write("\t%d\t" % nearest_downstream_gene_tup[0])
-		o_tss_file.write('|'.join(map(lambda tup: tup[1][:-1].replace('\t', ' '), nearest_downstream_gene_tup[1])))
-		o_tss_file.write('\n')
-	in_tss_file.close()
-	o_tss_file.close()
+        o_tss_file.write("\t%d\t" % nearest_upstream_gene_tup[0])
+        o_tss_file.write('|'.join(map(lambda tup: tup[1][:-1].replace('\t', ' '), nearest_upstream_gene_tup[1])))
+        o_tss_file.write("\t%d\t" % nearest_downstream_gene_tup[0])
+        o_tss_file.write('|'.join(map(lambda tup: tup[1][:-1].replace('\t', ' '), nearest_downstream_gene_tup[1])))
+        o_tss_file.write('\n')
+    in_tss_file.close()
+    o_tss_file.close()
+
+def main():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('in_tss_fn', metavar = '<input TSS file (0-based)>',
+                             help = 'TSS file has at least two fields, which are '
+                                    '0-based coordinate and strand from left '
+                                    'to right.')
+
+    arg_parser.add_argument('in_gff_fn', metavar = '<input reference GFF file>',
+    	                    help = 'Gene annotation file in GFF3 format. ')
+
+    arg_parser.add_argument('o_tss_ng_fn', metavar = '<output TSS nearest gene file>',
+    	                    help = 'Output file including the nearest upstream and '
+    	                           'downstream genes appended at the end of each line.')
+
+    args = arg_parser.parse_args()
+
+    ref_gff = GFF(args.in_gff_fn)
+
+    find_nearest_genes(args.in_tss_fn, args.o_tss_ng_fn, ref_gff)
+
+
 
 if __name__ == "__main__":
-	argv = sys.argv
-	if len(argv) != 4:
-		sys.stderr.write("Usage:\n%s <input TSS file (0 based)>\
- <input reference GFF file> <output TSS nearest gene file> \n" % argv[0])
-		sys.exit(2)
-
-	in_tss_fn = argv[1]
-	in_gff_fn = argv[2]
-	o_tss_fn = argv[3]
-
-	ref_gff = GFF(in_gff_fn)
-
-	find_nearest_genes(in_tss_fn, o_tss_fn, ref_gff)
-
+	main()
