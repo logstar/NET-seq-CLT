7692dcfea4d5986d6f03908dfaba0cddac8542d4
logstar <y.will.zhang@gmail.com>
2017-09-22 19:20:53 -0400

    refactored rev comp handling
    Use a new named tuple to store the original sequence.
    
    Query the target sequence, which is reversed, or complemented, or both.
    Output the original sequence.

--- src/count_fastq_match_len.py
+++ src/count_fastq_match_len.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python2.7
 import argparse
-import fastautil
 import dnautil
 import sequtil
 import fastqutil
+import collections
+
+# original seq is used to keep the original sequence without reverse / complement. 
+TargetSeqRecord = collections.namedtuple('FastaQuery', ['coord', 'num_up_bp', 'num_down_bp', 
+                                                        'strand', 'seq', 'original_seq'])
 
 # reverse and complement must be boolean
 def parse_fa_query_result_file(fa_qres_fn, reverse, complement):
     assert isinstance(reverse, bool)
     assert isinstance(complement, bool)
 
-    fa_qres_list = []
+    tseq_rec_list = []
     with open(fa_qres_fn, 'r') as fa_qres_file:
         for line in fa_qres_file:
             fields = line.strip().split()
@@ -19,6 +23,7 @@ def parse_fa_query_result_file(fa_qres_fn, reverse, complement):
             num_up_bp = int(fields[1])
             num_down_bp = int(fields[2])
             strand = fields[3]
+            original_seq = fields[4]
             target_seq = fields[4]
 
             if reverse:
@@ -27,13 +32,14 @@ def parse_fa_query_result_file(fa_qres_fn, reverse, complement):
             if complement:
                 target_seq = dnautil.complement(target_seq)
 
-            fa_qres = fastautil.FastaQueryResult(coord = coord, num_up_bp = num_up_bp, 
-                                                 num_down_bp = num_down_bp, 
-                                                 strand = strand, seq = target_seq)
+            tseq_rec = TargetSeqRecord(coord = coord, num_up_bp = num_up_bp, 
+                                      num_down_bp = num_down_bp, 
+                                      strand = strand, seq = target_seq, 
+                                      original_seq = original_seq)
 
-            fa_qres_list.append(fa_qres)
+            tseq_rec_list.append(tseq_rec)
 
-    return fa_qres_list
+    return tseq_rec_list
 
 
 def count_fastq_match_len(fq_fn, target_seq_set):
@@ -42,7 +48,7 @@ def count_fastq_match_len(fq_fn, target_seq_set):
     tseq_mlen_cnter_list = map(lambda tseq: sequtil.TargetSeqMatchLenCounter(tseq), target_seq_set)
 
     for seqid, seq, rseqid, qscore in fastqutil.iterate_fastq_file(fq_fn):
-        for tseq_cnter in tseq_mlen_cnter_list:
+        for tseq_mlen_cnter in tseq_mlen_cnter_list:
             tseq_mlen_cnter.add_query_seq(seq)
 
     return tseq_mlen_cnter_list
@@ -61,35 +67,27 @@ def all_same_mlen_range(tseq_mlen_cnt_list):
 
     return True
 
-def combine_fa_qres_tseq_mlen_cnt(fa_qres, tseq_mlen_cnt, reverse, complement):
-    coord = fa_qres.coord
-    strand = fa_qres.strand
-    seq = fa_qres.seq
+def combine_tseq_rec_tseq_mlen_cnt(tseq_rec, tseq_mlen_cnt):
+    coord = tseq_rec.coord
+    strand = tseq_rec.strand
+    seq = tseq_rec.seq
     tseq = tseq_mlen_cnt[0]
 
     assert seq.upper() == tseq.upper()
 
-    out_seq = seq
-    if reverse:
-        out_seq = out_seq[::-1]
-
-    if complement:
-        out_seq = dnautil.complement(out_seq)
+    out_seq = tseq_rec.original_seq
 
     mlen_cnt_tup = tuple(map(lambda t: t[1], tseq_mlen_cnt[1:]))
     
     return (coord, strand, out_seq) + mlen_cnt_tup
 
-# fa_qres_list is a list of fastautil.FastaQueryResult
+# tseq_rec_list is a list of TargetSeq
 # tseq_mlen_cnt_list is a list of tseq_mlen_cnt = [seq, (mlen0, mlen0_cnt), 
 #                                                  (mlen1, mlen1_cnt), (mlen0, mlen2_cnt), ...]
 # reverse or complement output sequence
-def fmt_fa_qres_mlen_cnt(fa_qres_list, tseq_mlen_cnt_list, reverse = False, complement = False):
-    assert isinstance(reverse, bool)
-    assert isinstance(complement, bool)
-    
-    if len(fa_qres_list) == 0:
-        raise ValueError('fa_qres_list is empty.')
+def fmt_tseq_rec_mlen_cnt(tseq_rec_list, tseq_mlen_cnt_list):
+    if len(tseq_rec_list) == 0:
+        raise ValueError('ftseq_rec_list is empty.')
     
     if len(tseq_mlen_cnt_list) == 0:
         raise ValueError('tseq_mlen_cnt_list is empty.')
@@ -97,28 +95,28 @@ def fmt_fa_qres_mlen_cnt(fa_qres_list, tseq_mlen_cnt_list, reverse = False, comp
     assert all_same_mlen_range(tseq_mlen_cnt_list), 'matched length ranges are different'
     mlen_tup = tuple(map(lambda t: t[0], tseq_mlen_cnt_list[0][1:]))
     
-    fa_qres_mlen_cnt_list = []
+    tseq_rec_mlen_cnt_list = []
 
     tseq_list = map(lambda l: l[0].upper(), tseq_mlen_cnt_list)
 
     assert len(tseq_list) == len(set(tseq_list)), 'target sequences are duplicated'
 
-    for fa_qres in fa_qres_list:
-        if fa_qres.seq.upper() not in tseq_list:
+    for tseq_rec in tseq_rec_list:
+        if tseq_rec.seq.upper() not in tseq_list:
             raise ValueError('Cannot find target seq.')
 
         for tseq_mlen_cnt in tseq_mlen_cnt_list:
-            if fa_qres.seq.upper() == tseq_mlen_cnt[0].upper():
-                fa_qres_mlen_cnt_list.append(
-                    combine_fa_qres_tseq_mlen_cnt(fa_qres, tseq_mlen_cnt, reverse, complement))
+            if tseq_rec.seq.upper() == tseq_mlen_cnt[0].upper():
+                tseq_rec_mlen_cnt_list.append(
+                    combine_tseq_rec_tseq_mlen_cnt(tseq_rec, tseq_mlen_cnt))
 
-    assert len(fa_qres_mlen_cnt_list) > 0
-    ncol = map(lambda t: len(t), fa_qres_mlen_cnt_list)
+    assert len(tseq_rec_mlen_cnt_list) > 0
+    ncol = map(lambda t: len(t), tseq_rec_mlen_cnt_list)
     assert len(set(ncol)) == 1
     assert ncol[0] == len(mlen_tup) + 3
     
     header = 'coord\tstrand\tseq\t' + '\t'.join(map(str, mlen_tup))
-    rows = map(lambda tup: '\t'.join(map(str, tup)), fa_qres_mlen_cnt_list)
+    rows = map(lambda tup: '\t'.join(map(str, tup)), tseq_rec_mlen_cnt_list)
 
     tbl_str = header + '\n' + '\n'.join(rows) + '\n'
 
@@ -152,17 +150,16 @@ def main():
     
     args = arg_parser.parse_args()
     
-    fa_qres_list = parse_fa_query_result_file(args.fa_qres_fn, reverse = args.reverse, 
-                                              complement = args.complement)
+    tseq_rec_list = parse_fa_query_result_file(args.fa_qres_fn, reverse = args.reverse, 
+                                               complement = args.complement)
 
-    target_seq_set = set(map(lambda fa_qres: fa_qres.seq.upper()))
+    target_seq_set = set(map(lambda tseq_rec: tseq_rec.seq.upper(), tseq_rec_list))
 
     tseq_mlen_cnter_list = count_fastq_match_len(args.fq_fn, target_seq_set)
 
-    tseq_mlen_cnt_list = map(lambda cnter: cnter.to_list(), tseq_mlen_cnter_list)
+    tseq_mlen_cnt_list = map(lambda cnter: cnter.to_list(0, 5), tseq_mlen_cnter_list)
 
-    o_tbl_str = fmt_fa_qres_mlen_cnt(fa_qres_list, tseq_mlen_cnt_list, reverse = args.reverse, 
-                                     complement = args.complement)
+    o_tbl_str = fmt_tseq_rec_mlen_cnt(tseq_rec_list, tseq_mlen_cnt_list)
 
     with open(args.o_fn, 'w') as ofile:
         ofile.write(o_tbl_str)
--- tests/test_count_fastq_match_len.py
+++ tests/test_count_fastq_match_len.py
@@ -3,7 +3,6 @@ import unittest
 import env
 import src.count_fastq_match_len as cml
 import src.sequtil as su
-import src.fastautil as fau
 
 class TestAllSameMlenRange(unittest.TestCase):
     def setUp(self):
@@ -36,7 +35,7 @@ class TestAllSameMlenRange(unittest.TestCase):
         self.assertTrue(cml.all_same_mlen_range([]))
 
 
-# fmt_fa_qres_mlen_cnt(fa_qres_list, tseq_mlen_cnt_list)
+# def fmt_tseq_rec_mlen_cnt(tseq_rec_list, tseq_mlen_cnt_list)
 class TestFmtFaQresMlenCnt(unittest.TestCase):
     def setUp(self):
         self.tseq_cnter_list = [su.TargetSeqMatchLenCounter('ACGTACG'),
@@ -50,73 +49,55 @@ class TestFmtFaQresMlenCnt(unittest.TestCase):
         self.tseq_diff_mlen_cnt_list = (map(lambda cnter: cnter.to_list(0, 100), self.tseq_cnter_list[:1]) 
             + map(lambda cnter: cnter.to_list(10, 20), self.tseq_cnter_list[1:]))
 
-        self.fa_qres_list = [fau.FastaQueryResult(0, 3, 3, '+', 'ACGtACG'),
-                             fau.FastaQueryResult(10, 3, 3, '-', 'AGGtACG'),
-                             fau.FastaQueryResult(20, 3, 3, '-', 'ATGtACG'),
-                             fau.FastaQueryResult(30, 4, 3, '+', 'ATTGtACG'),
-                             fau.FastaQueryResult(40, 4, 3, '+', 'ACCGtACG'),
-                             fau.FastaQueryResult(50, 3, 3, '-', 'ATGtACG'),
-                             fau.FastaQueryResult(60, 4, 3, '+', 'ACCGtACG'),
-                             fau.FastaQueryResult(70, 4, 3, '+', 'ACCGtACG')]
+        self.tseq_rec_list = [cml.TargetSeqRecord(0, 3, 3, '+', 'ACGtACG', 'X'),
+                              cml.TargetSeqRecord(10, 3, 3, '-', 'AGGtACG', 'Y'),
+                              cml.TargetSeqRecord(20, 3, 3, '-', 'ATGtACG', 'ATGtACG'),
+                              cml.TargetSeqRecord(30, 4, 3, '+', 'ATTGtACG', 'ATTGtACG'),
+                              cml.TargetSeqRecord(40, 4, 3, '+', 'ACCGtACG', 'ACCGtACG'),
+                              cml.TargetSeqRecord(50, 3, 3, '-', 'ATGtACG', 'ATGtACG'),
+                              cml.TargetSeqRecord(60, 4, 3, '+', 'ACCGtACG', 'ACCGtACG'),
+                              cml.TargetSeqRecord(70, 4, 3, '+', 'ACCGtACG', 'ACCGtACG')]
 
         # seqs not included in tseq_cnter_list
-        self.fa_qres_non_exist_list = [fau.FastaQueryResult(0, 3, 3, '+', 'CCCtGGG'),
-                                       fau.FastaQueryResult(0, 5, 5, '+', 'AAACCtACGAA')]
+        self.tseq_rec_non_exist_list = [cml.TargetSeqRecord(0, 3, 3, '+', 'CCCtGGG', 'CCCtGGG'),
+                                       cml.TargetSeqRecord(0, 5, 5, '+', 'AAACCtACGAA', 'AAACCtACGAA')]
 
                 
-    def test_empty_fa_qres_list(self):
+    def test_empty_tseq_rec_list(self):
         with self.assertRaises(ValueError):
-            cml.fmt_fa_qres_mlen_cnt([], self.tseq_same_mlen_cnt_list)
+            cml.fmt_tseq_rec_mlen_cnt([], self.tseq_same_mlen_cnt_list)
 
     def test_empty_tseq_mlen_cnt_list(self):
         with self.assertRaises(ValueError):
-            cml.fmt_fa_qres_mlen_cnt(self.fa_qres_list, [])
+            cml.fmt_tseq_rec_mlen_cnt(self.tseq_rec_list, [])
 
     def test_empty_both_input(self):
         with self.assertRaises(ValueError):
-            cml.fmt_fa_qres_mlen_cnt([], [])
+            cml.fmt_tseq_rec_mlen_cnt([], [])
 
     def test_diff_mlen(self):
         with self.assertRaises(AssertionError):
-            cml.fmt_fa_qres_mlen_cnt(self.fa_qres_list, self.tseq_diff_mlen_cnt_list)
+            cml.fmt_tseq_rec_mlen_cnt(self.tseq_rec_list, self.tseq_diff_mlen_cnt_list)
 
     def test_dup_tseq(self):
         with self.assertRaises(AssertionError):
-            cml.fmt_fa_qres_mlen_cnt(self.fa_qres_list, 
+            cml.fmt_tseq_rec_mlen_cnt(self.tseq_rec_list, 
                 self.tseq_same_mlen_cnt_list + self.tseq_same_mlen_cnt_list[:2])
 
-    def test_fa_qres_seq_not_in_tseq(self):
+    def test_tseq_rec_seq_not_in_tseq(self):
         with self.assertRaises(ValueError):
-            cml.fmt_fa_qres_mlen_cnt(self.fa_qres_non_exist_list, self.tseq_same_mlen_cnt_list)
+            cml.fmt_tseq_rec_mlen_cnt(self.tseq_rec_non_exist_list, self.tseq_same_mlen_cnt_list)
 
         with self.assertRaises(ValueError):
-            cml.fmt_fa_qres_mlen_cnt(self.fa_qres_non_exist_list + self.fa_qres_list, 
+            cml.fmt_tseq_rec_mlen_cnt(self.tseq_rec_non_exist_list + self.tseq_rec_list, 
                 self.tseq_same_mlen_cnt_list)
 
-    def test_invalid_r_arg(self):
-        with self.assertRaises(AssertionError):
-            cml.fmt_fa_qres_mlen_cnt(self.fa_qres_list, 
-                self.tseq_same_mlen_cnt_list + self.tseq_same_mlen_cnt_list[:2], 
-                reverse = 0)
-
-    def test_invalid_c_arg(self):
-        with self.assertRaises(AssertionError):
-            cml.fmt_fa_qres_mlen_cnt(self.fa_qres_list, 
-                self.tseq_same_mlen_cnt_list + self.tseq_same_mlen_cnt_list[:2], 
-                complement = 0)
-
-    def test_invalid_rc_arg(self):
-        with self.assertRaises(AssertionError):
-            cml.fmt_fa_qres_mlen_cnt(self.fa_qres_list, 
-                self.tseq_same_mlen_cnt_list + self.tseq_same_mlen_cnt_list[:2], 
-                reverse = 0, complement = 0)
-        
-    def test_output_format_implicit_args(self):
-        o_tbl_str = cml.fmt_fa_qres_mlen_cnt(self.fa_qres_list, self.tseq_same_mlen_cnt_list)
+    def test_output_format(self):
+        o_tbl_str = cml.fmt_tseq_rec_mlen_cnt(self.tseq_rec_list, self.tseq_same_mlen_cnt_list)
         ref_tbl_str = '\
 coord\tstrand\tseq\t0\t1\t2\t3\t4\t5\n\
-0\t+\tACGtACG\t0\t0\t0\t0\t0\t0\n\
-10\t-\tAGGtACG\t0\t0\t0\t0\t0\t0\n\
+0\t+\tX\t0\t0\t0\t0\t0\t0\n\
+10\t-\tY\t0\t0\t0\t0\t0\t0\n\
 20\t-\tATGtACG\t0\t0\t0\t0\t0\t0\n\
 30\t+\tATTGtACG\t0\t0\t0\t0\t0\t0\n\
 40\t+\tACCGtACG\t0\t0\t0\t0\t0\t0\n\
@@ -124,66 +105,6 @@ coord\tstrand\tseq\t0\t1\t2\t3\t4\t5\n\
 60\t+\tACCGtACG\t0\t0\t0\t0\t0\t0\n\
 70\t+\tACCGtACG\t0\t0\t0\t0\t0\t0\n'
         self.assertEqual(o_tbl_str, ref_tbl_str)
-
-    def test_output_format_explicit_args(self):
-        ref_tbl_str = '\
-coord\tstrand\tseq\t0\t1\t2\t3\t4\t5\n\
-0\t+\tACGtACG\t0\t0\t0\t0\t0\t0\n\
-10\t-\tAGGtACG\t0\t0\t0\t0\t0\t0\n\
-20\t-\tATGtACG\t0\t0\t0\t0\t0\t0\n\
-30\t+\tATTGtACG\t0\t0\t0\t0\t0\t0\n\
-40\t+\tACCGtACG\t0\t0\t0\t0\t0\t0\n\
-50\t-\tATGtACG\t0\t0\t0\t0\t0\t0\n\
-60\t+\tACCGtACG\t0\t0\t0\t0\t0\t0\n\
-70\t+\tACCGtACG\t0\t0\t0\t0\t0\t0\n'
-        o_tbl_str_exp = cml.fmt_fa_qres_mlen_cnt(self.fa_qres_list, self.tseq_same_mlen_cnt_list, 
-                                                 reverse = False, complement = False)
-        self.assertEqual(o_tbl_str_exp, ref_tbl_str)
-
-    def test_output_format_rev_seq(self):
-        ref_tbl_str_rev = '\
-coord\tstrand\tseq\t0\t1\t2\t3\t4\t5\n\
-0\t+\tGCAtGCA\t0\t0\t0\t0\t0\t0\n\
-10\t-\tGCAtGGA\t0\t0\t0\t0\t0\t0\n\
-20\t-\tGCAtGTA\t0\t0\t0\t0\t0\t0\n\
-30\t+\tGCAtGTTA\t0\t0\t0\t0\t0\t0\n\
-40\t+\tGCAtGCCA\t0\t0\t0\t0\t0\t0\n\
-50\t-\tGCAtGTA\t0\t0\t0\t0\t0\t0\n\
-60\t+\tGCAtGCCA\t0\t0\t0\t0\t0\t0\n\
-70\t+\tGCAtGCCA\t0\t0\t0\t0\t0\t0\n'
-        o_tbl_str_rev = cml.fmt_fa_qres_mlen_cnt(self.fa_qres_list, self.tseq_same_mlen_cnt_list, 
-                                                 reverse = True, complement = False)
-        self.assertEqual(o_tbl_str_rev, ref_tbl_str_rev)
-
-    def test_output_format_comp_seq(self):
-        ref_tbl_str_comp = '\
-coord\tstrand\tseq\t0\t1\t2\t3\t4\t5\n\
-0\t+\tTGCaTGC\t0\t0\t0\t0\t0\t0\n\
-10\t-\tTCCaTGC\t0\t0\t0\t0\t0\t0\n\
-20\t-\tTACaTGC\t0\t0\t0\t0\t0\t0\n\
-30\t+\tTAACaTGC\t0\t0\t0\t0\t0\t0\n\
-40\t+\tTGGCaTGC\t0\t0\t0\t0\t0\t0\n\
-50\t-\tTACaTGC\t0\t0\t0\t0\t0\t0\n\
-60\t+\tTGGCaTGC\t0\t0\t0\t0\t0\t0\n\
-70\t+\tTGGCaTGC\t0\t0\t0\t0\t0\t0\n'
-        o_tbl_str_comp = cml.fmt_fa_qres_mlen_cnt(self.fa_qres_list, self.tseq_same_mlen_cnt_list, 
-                                                  reverse = False, complement = True)
-        self.assertEqual(o_tbl_str_comp, ref_tbl_str_comp)
-
-    def test_output_format_rev_comp_seq(self):
-        ref_tbl_str_rev_comp = '\
-coord\tstrand\tseq\t0\t1\t2\t3\t4\t5\n\
-0\t+\tCGTaCGT\t0\t0\t0\t0\t0\t0\n\
-10\t-\tCGTaCCT\t0\t0\t0\t0\t0\t0\n\
-20\t-\tCGTaCAT\t0\t0\t0\t0\t0\t0\n\
-30\t+\tCGTaCAAT\t0\t0\t0\t0\t0\t0\n\
-40\t+\tCGTaCGGT\t0\t0\t0\t0\t0\t0\n\
-50\t-\tCGTaCAT\t0\t0\t0\t0\t0\t0\n\
-60\t+\tCGTaCGGT\t0\t0\t0\t0\t0\t0\n\
-70\t+\tCGTaCGGT\t0\t0\t0\t0\t0\t0\n'
-        o_tbl_str_rev_comp = cml.fmt_fa_qres_mlen_cnt(self.fa_qres_list, self.tseq_same_mlen_cnt_list, 
-                                                      reverse = True, complement = True)
-        self.assertEqual(o_tbl_str_rev_comp, ref_tbl_str_rev_comp)
 
 
 if __name__ == '__main__':
